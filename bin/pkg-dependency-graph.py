397763079995b0e0fd50222c6da19d425c40baaa
Adriaan de Groot <groot@kde.org>
2018-09-06 21:04:46 +0200

    Script to print out a graph of package dependencies. Needs dot to turn the output into a nice picture.
    At the top of the graph, comments are printed identifying
    packages which are "roots" of the forest of installed
    packages -- these are the things that you have explicitly
    installed (or, alternatively, which may be candidates
    for autoremove).

---
+++ bin/pkg-dependency-graph.py
@@ -0,0 +1,110 @@
+#! /usr/bin/env python
+#
+# A script to generate a .dot file (Graphviz) from installed
+# packages, to show what depends on what.
+
+import os
+import subprocess
+import sys
+
+if sys.version_info < (3, 5):
+    raise ValueError("Python 3.5 or later is required.")
+
+def collect_pkg():
+    """
+    Run pkg, and returns a dictionary
+    where the keys are names of packages,
+    and the values are lists of packages that the key depends on.
+    """
+    deps = {}
+    PKG = "/usr/sbin/pkg"
+    QUERY = "query"
+
+    # Gather all packages, init deps to an empty dependency list per package
+    r = subprocess.run(
+        [PKG, QUERY, "%o"],
+        stdout=subprocess.PIPE)
+    output = r.stdout.split(b"\n")
+    for line in output:
+        if line:
+            line = line.decode("ascii")
+            deps[line] = []
+
+    # Now build up the dependency graph
+    r = subprocess.run(
+        [PKG, QUERY, "%o::%do"], 
+        stdout=subprocess.PIPE)
+    output = r.stdout.split(b"\n")
+    for line in output:
+        if line:
+            line = line.decode("ascii")
+            package, dep = line.split("::")
+            deps[package].append(dep)
+    
+    return deps
+    
+def normalize_deps(deps):
+    """
+    Normalize the dependencies.
+    
+    A package in @p deps now has a dependency list that contains
+    all the transitive dependencies; prune it down to only the
+    first-level dependencies.
+    """
+    normalized_deps = {}
+    for key in deps.keys():
+        all_deps = set(deps[key])
+        second_deps = set()
+        for k in all_deps:
+            second_deps.update(set(deps[k]))
+
+        l = list(all_deps - second_deps)
+        l.sort()
+        
+        normalized_deps[key] = l
+        
+    return normalized_deps
+
+def count_reverse(deps):
+    """
+    For a dependency graph pkg -> [pkgs], returns a dictionary
+    with pkg as key, and the number of *other* packages that depend
+    on this one. May be done with a normalized, or not-normalized graph.
+    
+    The keys that keep count 0 are the "root" nodes of the forest of
+    installed packages.
+    """
+    reverse_count = {}
+    for key in deps.keys():
+        reverse_count[key] = 0
+    for key in deps.keys():
+        for d in deps[key]:
+            # These are the packages that k depends on, so they all get +1
+            reverse_count[d] += 1
+            
+    return reverse_count
+    
+    
+if __name__ == "__main__":
+    dependency_graph = normalize_deps(collect_pkg())
+    packages = list(dependency_graph.keys())
+    packages.sort()
+
+
+    print("### Root nodes:\n###\n#")
+    count = count_reverse(dependency_graph)
+    for k in packages:
+        if count[k] == 0:
+            print("#  p%d %s" % (packages.index(k), k))
+            
+    print("digraph {")
+    for k in packages:
+        print("  p%d [label=\"%s\"];" % (packages.index(k), str(k)))
+        
+    c = 1
+    for k in packages:
+        for dep in dependency_graph[k]:
+            print("  p%d -> p%d;" % (packages.index(k), packages.index(dep)) )
+            
+    print("}")
+    
